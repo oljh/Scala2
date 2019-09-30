c36b5c3465e987084f707e6de591b3b648388832
oljh <34842956+oljh@users.noreply.github.com>
2019-09-30 23:47:27 +0300

    revers2

--- src/main/scala/Tasks/ReverseOddWord.scala
+++ src/main/scala/Tasks/ReverseOddWord.scala
@@ -6,17 +6,14 @@ object ReverseOddWord {
 
   def main(args: Array[String]): Unit = {
 
-
     val rex: Regex = "\\w+".r
-
-    val rtext: String = "!awe%some&text*password!"
-
+    val rtext: String = "!awe%sOme&teXt*password!"
     var l: List[String] = (rex findAllIn rtext).toList
     println(l)
 
     for (k <- 0 until l.size) {
       print(" ")
-      if (k % 2 != 0) {
+      if (k % 2 == 0) {
         print(l(k).reverse)
       }
       else print(l(k))

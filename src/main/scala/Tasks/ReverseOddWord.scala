95e09e86d08c7ab6276745dca04da69c80b51bb3
oljh <https://github.com/oljh/MyCheese.git>
2019-10-01 17:05:38 +0300

    devel_ revers

--- src/main/scala/Tasks/ReverseOddWord.scala
+++ src/main/scala/Tasks/ReverseOddWord.scala
@@ -7,8 +7,9 @@ object ReverseOddWord {
   def main(args: Array[String]): Unit = {
 
     val rex: Regex = "\\w+".r
-    val rtext: String = "!awe%sOme&teXt*password!"
-    var l: List[String] = (rex findAllIn rtext).toList
+    val sentence: String = "!awe%sOme&teXt*password!"
+
+    var l: List[String] = (rex findAllIn sentence).toList
     println(l)
 
     for (k <- 0 until l.size) {
@@ -19,8 +20,58 @@ object ReverseOddWord {
       else print(l(k))
     }
 
-
+    oRev(sentence)
   }
 
+  def oRev(str: String): Unit = {
+    var count: Int = 0
+    var boo: Boolean = false
+    var result: String = ""
+    for (s <- str) {
+
+      if (Character.isAlphabetic(s) & (count % 2) == 0) {
+        boo = true
+        result = result + s
+
+      } else if (Character.isAlphabetic(s) & (count % 2) != 0) {
+        result =  s + result
+        boo = true}
+
+      if (!Character.isAlphabetic(s) & boo == true) {
+        count = count + 1
+        result = result + s
+        boo == false
+
+      }
+    }
+    print(result)
+  }
+
+
+  /*
+
+    def oddRev(str: String): String = {
+
+
+      def rev(str: String, count: Int, rStr: String, nW: Boolean): String = {
+
+        if (Character.isAlphabetic(str.head) & (count % 2) != 0) {
+          rev(str.tail, count, rStr + str.head, true)
+        }
+        else if (Character.isAlphabetic(str.head) & (count % 2) == 0) {
+          rev(str.tail, count, str.head + rStr, true)
+
+        }
+
+        else if (!Character.isAlphabetic(str.head) & nW == true) {
+          rev(str.tail, count + 1, rStr + str.head, false)
+
+        }
+
+      }
+
+      rev(str, 0, "", false)
+    }
+  */
 
 }

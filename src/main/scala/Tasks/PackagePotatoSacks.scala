dcadf86b1f86a3bac397805e7cc3c9d8db5166d4
oljh <https://github.com/oljh/MyCheese.git>
2019-10-03 16:44:50 +0300

    potato

--- src/main/scala/Tasks/PackagePotatoSacks.scala
+++ src/main/scala/Tasks/PackagePotatoSacks.scala
@@ -12,6 +12,7 @@ object PackagePotatoSacks {
 
   def packagePotato(big: Int, small: Int, goal: Int): Boolean = {
     if (goal == 0) true
+    else if (goal == 0 & (small >= 0 || big >= 0)) true
     else if ((big > 0) & (goal >= big * 5)) packagePotato(big - 1, small, goal - 5)
     else if ((small > 0) & (goal > 0)) packagePotato(big, small - 1, goal - 1)
     else false

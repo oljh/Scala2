4498c085cb6a7000a89091df6bfddcc7b5dc0859
oljh <34842956+oljh@users.noreply.github.com>
2019-10-03 21:25:47 +0300

    poteto and reverse

--- src/main/scala/Tasks/PackagePotatoSacks.scala
+++ src/main/scala/Tasks/PackagePotatoSacks.scala
@@ -11,9 +11,8 @@ object PackagePotatoSacks {
   }
 
   def packagePotato(big: Int, small: Int, goal: Int): Boolean = {
-    if (goal == 0) true
-    else if (goal == 0 & (small >= 0 || big >= 0)) true
-    else if ((big > 0) & (goal >= big * 5)) packagePotato(big - 1, small, goal - 5)
+    if ((big >= 0) & (small >= 0) & (goal == 0)) true
+    else if ((big > 0) & (goal >5)) packagePotato(big - 1, small, goal - 5)
     else if ((small > 0) & (goal > 0)) packagePotato(big, small - 1, goal - 1)
     else false
   }
--- src/main/scala/Tasks/ReverseOddWords.scala
+++ src/main/scala/Tasks/ReverseOddWords.scala
@@ -1,28 +1,19 @@
 package Tasks
 
-import scala.util.matching.Regex
-
 object ReverseOddWords {
 
   def main(args: Array[String]): Unit = {
-    var sentence: String = "!awe%sOme&teXt*password!simple^sOme"
-    var sentence2: String = "just some simple sentence"
+    val sentence: String = "just some simple sentence"
     println(sentence)
     println(revOddWord(sentence))
-    println(sentence2)
-    println(revOddWord(sentence2))
   }
 
   def revOddWord(str: String): String = {
-    val rex: Regex = "\\w+".r
-    var list: List[String] = (rex findAllIn str).toList
+    val list = str.split("\\W+").toList
     var sentence = str
-    for (i <- 0 until list.size) {
-      if ((i & 1) == 0) {
-        sentence = sentence.replaceFirst(list(i), list(i).reverse)
-      }
+    for (i <- list.indices) {
+      if (i % 2 == 0) sentence = sentence.replaceFirst(list(i), list(i).reverse)
     }
     sentence
   }
-
 }

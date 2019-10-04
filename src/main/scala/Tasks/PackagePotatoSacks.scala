c213db1830ac234c4d46cfd20b22601ad7f5a1f0
oljh <https://github.com/oljh/MyCheese.git>
2019-10-04 17:19:50 +0300

    potato

--- src/main/scala/Tasks/PackagePotatoSacks.scala
+++ src/main/scala/Tasks/PackagePotatoSacks.scala
@@ -4,16 +4,26 @@ object PackagePotatoSacks {
 
   def main(args: Array[String]): Unit = {
 
-    println(packagePotato(1, 1, 6))
-    println(packagePotato(1, 5, 11))
-    println(packagePotato(2, 1, 9))
+    println(packagePotato2(1, 1, 6))
+    println(packagePotato2(1, 5, 11))
+    println(packagePotato2(2, 1, 9))
 
   }
 
   def packagePotato(big: Int, small: Int, goal: Int): Boolean = {
-    if ((big >= 0) & (small >= 0) & (goal == 0)) true
-    else if ((big > 0) & (goal >5)) packagePotato(big - 1, small, goal - 5)
-    else if ((small > 0) & (goal > 0)) packagePotato(big, small - 1, goal - 1)
+    if ((big >= 0) && (small >= 0) && (goal == 0)) true
+    else if ((big > 0) && (goal > 5)) packagePotato(big - 1, small, goal - 5)
+    else if ((small > 0) && (goal > 0)) packagePotato(big, small - 1, goal - 1)
     else false
   }
+/*
+  Try not to use recursion, the end solution should be one-lined if-else statement with some math:
+
+  if (big * 5 >= ...) ... >= ... % 5 else ... >= goal - ... * ...
+
+*/
+  def packagePotato2(big: Int, small: Int, goal: Int): Boolean = {
+    if (big * 5 >= goal) big >= goal % 5 else if (small >= goal) small >= goal % 1 else false
+}
+
 }

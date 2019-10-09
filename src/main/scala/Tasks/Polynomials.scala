00a63b4392b4dd218691fd017a018225e8efddd1
oljh <https://github.com/oljh/MyCheese.git>
2019-10-09 17:00:50 +0300

    polynomials

--- src/main/scala/Tasks/Polynomials.scala
+++ src/main/scala/Tasks/Polynomials.scala
@@ -1,65 +1,95 @@
 package Tasks
 
 object Polynomials extends App {
-
-  println(new Poly(5.9, 1.5, 2.3, -3.8))
+  var pol = new Polynomial(5.9, 1.5, 2.3, -3.8)
+  println(pol)
+  println(pol.evaluate(1))
 
 }
 
-class Poly(coef: Double*) {
+class Polynomial(coefficients: Double*) {
 
-  private val ArCof = crCoeffs(coef.toArray)
+  private val arrCoef = coefToArray(coefficients.toArray)
 
 
-  private def crCoeffs(arrCof: Array[Double]): Array[Double] = {
+  private def coefToArray(arrCof: Array[Double]): Array[Double] = {
     var s = arrCof.length - 1
     while (s >= 0 && arrCof(s) == 0)
       s -= 1
     arrCof take (s + 1)
   }
 
-  def degree: Int = ArCof.length - 1
+  def getDegree: Int = arrCoef.length - 1
 
-  def coeff(i: Int): Double = if (i < ArCof.length) ArCof(i) else 0
+  def coef(i: Int): Double = if (i < arrCoef.length) arrCoef(i) else 0
 
   override def toString: String = {
-    for (i <- ArCof.indices) {
-      print(ArCof(i))
+    for (i <- arrCoef.indices) {
+      print(arrCoef(i))
     }
     println
-
-    var s = new StringBuilder
+    var result = new StringBuilder
     var plus = ""
-    for (i <- degree to 0 by -1) {
-      if (coeff(i) != 0) {
-        var c = coeff(i)
-        s ++= (if (c > 0) plus else "-")
+    for (i <- getDegree to 0 by -1) {
+      if (coef(i) != 0) {
+        var cf = coef(i)
+        result ++= (if (cf > 0) plus else "-")
         plus = "+"
-        c = c.abs
+        cf = cf.abs
         if (i == 0)
-          s ++= c.toString
+          result ++= cf.toString
         else {
-          if (c != 1)
-            s ++= c.toString + ""
+          if (cf != 1)
+            result ++= cf.toString + ""
           if (i > 1)
-            s ++= "x^" + i.toString
+            result ++= "x^" + i.toString
           else
-            s ++= "x"
+            result ++= "x"
         }
       }
     }
-    s.toString
+
+
+    result.toString
+
   }
 
-  // evaluate this polynomial at x
   def evaluate(x: Double): Double = {
     var result = 0.0
-    for (i <- 0 to degree) {
-      var power = 1.0
+    for (i <- 0 to getDegree) {
+      var k = 1.0
       for (j <- 1 to i)
-        power *= x
-      result += coeff(i) * power
+        k *= x
+      result += coef(i) * k
     }
     result
   }
+
+  def add(another: Polynomial): Polynomial = {
+    val deg = getDegree max another.getDegree
+    val R = new Array[Int](deg + 1)
+    for (i <- 0 to deg)
+      R(i) = coef(i) + another.coef(i)
+    new Polynomial(R)
+  }
+
+  def multiple(another: Polynomial): Polynomial = {
+    // first handle case if one factor is the zero polynomial
+    if (getDegree < 0)
+      this
+    else if (another.getDegree< 0)
+      another
+    else {
+      val deg = getDegree + another.getDegree
+      val R = new Array[Int](deg + 1)
+      for (i <- 0 to deg) {
+        var sum = 0
+        for (j <- 0 to i)
+          sum += coef(j) * another.coef(i - j)
+        R(i) = sum
+      }
+      new Polynomial(R)
+    }
+  }
+
 }

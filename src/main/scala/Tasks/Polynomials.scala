bbfa6540bfaf7c202ae68c00b9270630271f8dfc
oljh <34842956+oljh@users.noreply.github.com>
2019-10-09 23:04:42 +0300

    poteto and reverse

--- src/main/scala/Tasks/Polynomials.scala
+++ src/main/scala/Tasks/Polynomials.scala
@@ -3,20 +3,20 @@ package Tasks
 object Polynomials extends App {
   var pol = new Polynomial(5.9, 1.5, 2.3, -3.8)
   println(pol)
-  println(pol.evaluate(1))
 
+  println(pol.evaluate(1))
+  print(pol.add(new Polynomial(7.0,1.2,3.3,4.6,5.6,6.5,7.5,8.3)))
 }
 
 class Polynomial(coefficients: Double*) {
 
-  private val arrCoef = coefToArray(coefficients.toArray)
+  private val arrCoef = coefficients
 
-
-  private def coefToArray(arrCof: Array[Double]): Array[Double] = {
-    var s = arrCof.length - 1
-    while (s >= 0 && arrCof(s) == 0)
+  private def createCoeffs(A: Array[Int]): Array[Int] = {
+    var s = A.length - 1
+    while (s >= 0 && A(s) == 0)
       s -= 1
-    arrCof take (s + 1)
+    A take (s+1)
   }
 
   def getDegree: Int = arrCoef.length - 1
@@ -67,28 +67,26 @@ class Polynomial(coefficients: Double*) {
 
   def add(another: Polynomial): Polynomial = {
     val deg = getDegree max another.getDegree
-    val R = new Array[Int](deg + 1)
-    for (i <- 0 to deg)
-      R(i) = coef(i) + another.coef(i)
-    new Polynomial(R)
+    val result= Array[Double](8)
+    for (i <- 1 to 7){ print(result(i) = coef(i) + another.coef(i))}
+    new Polynomial(result:_*)
   }
 
   def multiple(another: Polynomial): Polynomial = {
-    // first handle case if one factor is the zero polynomial
     if (getDegree < 0)
       this
-    else if (another.getDegree< 0)
+    else if (another.getDegree < 0)
       another
     else {
       val deg = getDegree + another.getDegree
-      val R = new Array[Int](deg + 1)
+      val result= Array[Double](deg+1)
       for (i <- 0 to deg) {
-        var sum = 0
+        var sum = 0.0
         for (j <- 0 to i)
           sum += coef(j) * another.coef(i - j)
-        R(i) = sum
+        result(i) = sum
       }
-      new Polynomial(R)
+      new Polynomial(result:_*)
     }
   }
 

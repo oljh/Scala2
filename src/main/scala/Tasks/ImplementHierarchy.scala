33e4d56365df48f1ed35d59876539d72c99ee00a
oljh <34842956+oljh@users.noreply.github.com>
2019-10-14 00:43:46 +0300

    polinomials

--- src/main/scala/Tasks/ImplementHierarchy.scala
+++ src/main/scala/Tasks/ImplementHierarchy.scala
@@ -1,35 +1,48 @@
 package Tasks
-/*
+
 object ImplementHierarchy {
-
-  trait GeometricObject {
-    def perimeter(per: Double) {}
-
-    def area(ar: Double) {}
-  }
 
 }
 
-class Shape(color: String, filled: Boolean) {
+trait GeometricObject {
+  def perimeter(per: Double)
+  def area(ar: Double)
+}
+
+abstract class Shape(color: String, filled: Boolean) extends GeometricObject {
+
+  def perimeter(per: Double): Unit = ???
+
+  def area(ar: Double): Unit = ???
 
   def color(c: String): Shape
-
   def filled(f: Boolean): Shape
 }
 
 
-class Circle(radius: Double) {
-  def radius(r: Double): Circle
+class Circle(c:String, f:Boolean, radius: Double) extends Shape (c:String, f:Boolean) {
+
+  def radius(r: Double): Circle = ???
+
+  override def color(c: String): Shape = ???
+  override def filled(f: Boolean): Shape = ???
 }
 
-class Rectangle(width: Double, length: Double) {
-  def width(w: Double): Rectangle
+class Rectangle(color:String, filled:Boolean, width: Double, length: Double) extends Shape(color:String, filled:Boolean){
 
-  def lenth(l: Double): Rectangle
+  def width(w: Double): Rectangle = this
+  def length(l: Double): Rectangle = this
+
+  override def color(c: String): Shape = ???
+  override def filled(f: Boolean): Shape = ???
 }
 
-class Square(side: Double) {
-  def side(s: Double): Square
+class Square(color:String, filled:Boolean, width: Double, length: Double, side: Double) extends Rectangle(color:String, filled:Boolean, width: Double, length: Double){
+  override def width(w: Double): Rectangle = super.width(side)
+
+  override def length(l: Double): Rectangle = super.length(side)
+
+  def side(s: Double): Square = ???
 }
 
-*/
+
